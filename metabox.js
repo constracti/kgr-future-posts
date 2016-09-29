f053f1c67d8ebbdfc4b992b9d73a8c9d1c4883a0
Const R <constracti@gmail.com>
2016-09-29 03:43:42 +0300

    add post meta box

---
+++ metabox.js
@@ -0,0 +1,32 @@
+jQuery( function() {
+
+jQuery( '#postcal-check' ).change( function() {
+	var check = jQuery( this );
+	if ( check.prop( 'checked' ) )
+		jQuery( '#postcal-container' ).show();
+	else
+		jQuery( '#postcal-container' ).hide();
+} ).change();
+
+jQuery( '#postcal-save' ).click( function() {
+	var link = jQuery( this );
+	var check = jQuery( '#postcal-check' );
+	var input = jQuery( '#postcal-date' );
+	var spinner = jQuery( '#postcal-spinner' ).addClass( 'is-active' );
+	var data = {};
+	if ( check.prop( 'checked' ) )
+		data.postcal_check = check.val();
+	data.postcal_date = input.val();
+	jQuery.post( link.prop( 'href' ), data, function( data ) {
+		if ( typeof( data ) === 'object' ) {
+			check.prop( 'checked', data.check ).change();
+			input.val( data.date );
+		} else {
+			alert( data );
+		}
+		spinner.removeClass( 'is-active' );
+	} );
+	return false;
+} );
+
+} );
